d334e375d172a0a3977f4e69bb3b0d226b14b306
knlio <yasmin.7ussen@gmail.com>
2023-08-12 14:02:20 +0300

    first commit

--- mycoach.py
+++ mycoach.py
@@ -1,6 +1,6 @@
 import os
 import openai
-import streamlit
+import streamlit as st
 openai.api_key = os.getenv("OPENAI_API_KEY")
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
@@ -21,3 +21,25 @@ response = openai.ChatCompletion.create(
   presence_penalty=0
 )
 
+st.header("STREAMLIT GPT-3 CHATBOT")
+
+text = st.empty()
+show_messages(text)
+
+prompt = st.text_input("Prompt", value="Enter your message here...")
+
+if st.button("Send"):
+    with st.spinner("Generating response..."):
+        st.session_state["messages"] += [{"role": "user", "content": prompt}]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=st.session_state["messages"]
+        )
+        message_response = response["choices"][0]["message"]["content"]
+        st.session_state["messages"] += [
+            {"role": "system", "content": message_response}
+        ]
+        show_messages(text)
+
+if st.button("Clear"):
+    st.session_state["messages"] = BASE_PROMPT
+    show_messages(text)
